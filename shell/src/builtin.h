7410440b0021188176c0697aea0d797f25642223
tao <ts202115@ohio.edu>
2020-05-08 12:20:30 -0400

    Removed redundant high-level folder.

--- shell/src/builtin.h
+++
@@ -1,5 +0,0 @@
-extern int builtinCD(char **);
-extern int builtinPWD(char **);
-extern int builtinSET(char **);
-extern int builtinEXPORT(char **);
-extern int builtinEXIT(char **);
--- shell/src/job.h
+++
@@ -1,5 +0,0 @@
-#define PIPERD 0
-#define PIPEWR 1
-
-extern int exeExtCmd(struct CommandData *);
-extern void launchProcess(struct Command *, int, int, int);
